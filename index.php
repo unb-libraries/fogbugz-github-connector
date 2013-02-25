c8c19bd98237951835ccdc8669053f24d8acd2ca
Jake Sanford <jsanford@unb.ca>
2013-02-25 15:03:46 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Leverages Github pushes via service hooks to a fogbugz Email account, placing the commit notifications in the enumerated ticket history.
+ * As BugzID numbers are unique across projects, this does not require any configuration on the fogbugz instance.
+ */
+$fogBugzEmail='';
+$replyToEmail='';
+
+try {
+	$payload = json_decode($_REQUEST['payload']);
+} catch(Exception $e) {
+	exit(0);
+}
+
+preg_match( '/.*Bug[sz]id *: *(\d{1,5})/i', $payload->head_commit->message, $match );
+
+if ( $match[1] > 0 ) {
+	$mailSubject = '(Case ' . $match[1] . ') ' . $payload->head_commit->message;
+	$mailMessage = "Commit Pushed\n----------\nAuthor : " . $payload->head_commit->author->email . "\nMessage : " . $payload->head_commit->message."\nURL : ".$payload->head_commit->url;
+	$to = $fogBugzEmail;
+	$headers =	'From: ' . $replyToEmail . "\r\n" .
+				'Reply-To: ' . $replyToEmail . "\r\n" .
+				'X-Mailer: PHP/' . phpversion();
+	mail($to, $mailSubject, $mailMessage, $headers);
+}
