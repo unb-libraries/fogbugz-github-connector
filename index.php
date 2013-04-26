60b6dd253c9ae692d3b7a0d0dcc6ec8c83f2a845
Jacob Sanford <jsanford@unb.ca>
2013-04-26 09:42:57 -0300

    Changes to reflect logging and live usage

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  */
 $fogBugzEmail='';
 $replyToEmail='';
+$logFile='';
 
 try {
 	$payload = json_decode($_REQUEST['payload']);
@@ -17,9 +18,10 @@ preg_match( '/.*Bug[sz]id *: *(\d{1,5})/i', $payload->head_commit->message, $mat
 if ( $match[1] > 0 ) {
 	$mailSubject = '(Case ' . $match[1] . ') ' . $payload->head_commit->message;
 	$mailMessage = "Commit Pushed\n----------\nAuthor : " . $payload->head_commit->author->email . "\nMessage : " . $payload->head_commit->message."\nURL : ".$payload->head_commit->url;
-	$to = $fogBugzEmail;
-	$headers =	'From: ' . $replyToEmail . "\r\n" .
-				'Reply-To: ' . $replyToEmail . "\r\n" .
-				'X-Mailer: PHP/' . phpversion();
+	$to      = $fogBugzEmail;
+	$headers =		'From: ' . $replyToEmail  . "\r\n" .
+					'Reply-To: ' . $replyToEmail . "\r\n" .
+					'X-Mailer: PHP/' . phpversion();
+	file_put_contents($logFile, "\n".$mailMessage."\n".$to."\n".$headers, FILE_APPEND);
 	mail($to, $mailSubject, $mailMessage, $headers);
 }
