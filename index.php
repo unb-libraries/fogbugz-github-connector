60d6c96f9071168ca55d167a0a028b3e64815f28
Jacob Sanford <jsanford@unb.ca>
2013-04-30 13:42:30 -0300

    Parse all commits in payload, not just HEAD commit

--- index.php
+++ index.php
@@ -13,15 +13,17 @@ try {
 	exit(0);
 }
 
-preg_match( '/.*Bug[sz]id *: *(\d{1,5})/i', $payload->head_commit->message, $match );
-
-if ( $match[1] > 0 ) {
-	$mailSubject = '(Case ' . $match[1] . ') ' . $payload->head_commit->message;
-	$mailMessage = "Commit Pushed\n----------\nAuthor : " . $payload->head_commit->author->email . "\nMessage : " . $payload->head_commit->message."\nURL : ".$payload->head_commit->url;
-	$to      = $fogBugzEmail;
-	$headers =		'From: ' . $replyToEmail  . "\r\n" .
-					'Reply-To: ' . $replyToEmail . "\r\n" .
-					'X-Mailer: PHP/' . phpversion();
-	file_put_contents($logFile, "\n".$mailMessage."\n".$to."\n".$headers, FILE_APPEND);
-	mail($to, $mailSubject, $mailMessage, $headers);
+foreach ($payload->commits as $current_commit) {
+	$match=array();
+	preg_match( '/.*Bug[sz]id *: *(\d{1,5})/i', $current_commit->message, $match );
+	if ( $match[1] > 0 ) {
+		$mailSubject = '(Case ' . $match[1] . ') ' . $current_commit->message;
+		$mailMessage = "Commit Pushed\n----------\nAuthor : " . $current_commit->author->email . "\nMessage : " . $current_commit->message."\nURL : ".$current_commit->url;
+		$to      = $fogBugzEmail;
+		$headers =		'From: ' . $replyToEmail  . "\r\n" .
+						'Reply-To: ' . $replyToEmail . "\r\n" .
+						'X-Mailer: PHP/' . phpversion();
+		file_put_contents($logFile, "\n".$mailMessage."\n".$to."\n".$headers, FILE_APPEND);
+		mail($to, $mailSubject, $mailMessage, $headers);
+	}
 }
